85bbed9822daa4b81eb9aedbc3018a5dd6189519
= <ganna.andrushko@gmail.com>
2023-06-20 13:02:13 +0800

    edit redirect uri gain

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,6 +1,6 @@
 let accessToken;
 let clientId = "43ca929e8f054debbd76d1a9c5a39f94";
-let redirect_URI = "https://music-app-delta-han.vercel.app/callback";
+let redirect_URI = "music-h1ccf7ln8-ganna-and.vercel.app/callback";
 
 const Spotify = {
   getAccessToken() {
